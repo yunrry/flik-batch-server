d6550a70b0039aea8153f30e55dcec372460b3fe
yunrry <yunryy329@gmail.com>
2025-09-08 13:51:07 +0900

    init

---
+++ src/main/java/yunrry/flik/batch/FlikBatchApplication.java
@@ -0,0 +1,13 @@
+package yunrry.flik.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlikBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlikBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yunrry/flik/batch/FlikBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package yunrry.flik.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlikBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
